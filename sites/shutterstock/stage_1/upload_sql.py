099e2de8fa53bad1d4e1a110a890a947a79147d9
chavinlo <85657083+chavinlo@users.noreply.github.com>
2023-07-15 22:18:14 +0000

    5:18 PM

--- sites/shutterstock/stage_1/upload_sql.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import HfApi
-
-api = HfApi()
-api.upload_file(
-    path_or_fileobj="/home/ubuntu/tempofunk-scrapper/sites/shutterstock/stage_1/backup.db",
-    path_in_repo="sql/database.db",
-    repo_id="shinonomelab/cleanvid-15m_map",
-    repo_type="dataset",
-)
