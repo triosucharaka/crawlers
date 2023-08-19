a131441873563c5593201bb7dcebcec8d3143863
chavinlo <85657083+chavinlo@users.noreply.github.com>
2023-08-19 07:22:08 +0000

    shell noinput

--- scripts/mountbucket.sh
+++ scripts/mountbucket.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# No input
+export DEBIAN_FRONTEND=noninteractive
+
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
